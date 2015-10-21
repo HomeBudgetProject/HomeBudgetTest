c8467d3ed81c389df64f536753d374e540d50e38
Xangel2008 <Xangel2008@gmail.com>
2015-10-21 11:41:31 +0300

    rewrite acceptance test

--- acceptance_test/src/test/java/ua/com/ua/com/homebudget/jbehave/DefinitionSteps.java
+++ acceptance_test/src/test/java/ua/com/ua/com/homebudget/jbehave/DefinitionSteps.java
@@ -6,6 +6,10 @@ import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
 import ua.com.ua.com.homebudget.steps.EndUserSteps;
+import ua.com.ua.com.homebudget.steps.Login.LoginSteps;
+import ua.com.ua.com.homebudget.steps.Registrarion.RegistrationSteps;
+import ua.com.ua.com.homebudget.steps.ResetPassword.ResetPasswordSteps;
+
 
 /**
  * Created by Anohin Artyom on 10.09.2015.
@@ -16,31 +20,51 @@ public class DefinitionSteps {
     @Steps
     EndUserSteps endUser;
 
-    @Given("open registration page")
-    public void openRegistrationPage() {endUser.openRegisterPage();}
-
-    @When("enter email '$email' password '$pass' and send")
-    public void enterEmailPasswordRegistrationPage(String email, String pass){endUser.registration(email, pass);}
-
     @Then("redirect to main page")
-    public void redirectMainPage(){endUser.redirectMainPage();}
+    public void isOnMainPage(){endUser.isOnMainPage();}
+
+
+    @Steps
+    LoginSteps loginSteps;
+
+    @Given("open login page")
+    public void openLoginPage(){loginSteps.openLoginPage();}
+
+    @When("enter email '$email' password '$pass' in Login page and send")
+    public void enterEmailPasswordLoginPage(String email, String pass){loginSteps.login(email, pass);}
 
     @Then("in menu displays the caption '$email' in page")
-    public void verifyCredentials(String email){endUser.verifyCredentials(email);}
+    public void verifyCredentials(String email){loginSteps.verifyCredentials(email);}
 
-    //Login section
-    @Given("open login page")
-    public void openLoginPage(){endUser.openLoginPage();}
 
-    @When("enter email '$email' password '$pass' in Login page")
-    public void enterEmailPasswordLoginPage(String email, String pass){endUser.login(email, pass);}
+    @Steps
+    RegistrationSteps registrationSteps;
 
-    //Resset passwoed section
-    @Given("open resset password page")
-    public void openRessetPage(){endUser.openRessetPage();}
+    @Given("open registration page")
+    public void openRegistrationPage() {registrationSteps.openRegisterPage();}
+
+    @When("enter email '$email' password '$pass'") //for validate form
+    public void enterEmailPasswordRegistrationPage(String email, String pass) {registrationSteps.validate(email, pass);}
+
+    @When("enter email '$email' password '$pass' and send") //for create record
+    public void enterEmailPasswordSendRegistrationPage(String email, String pass){registrationSteps.registration(email, pass);}
+
+
+    @Then("show warning notification '$message' on Register page")
+    public void verifyGeneralWarningAtRegistration(String message){registrationSteps.verifyGeneralRegisterWarning(message);}
+
+    @Then("show password warning notification '$message' on Register page")
+    public void verifyPasswordWarningAtRegistration(String message){registrationSteps.verifyPasswordWarningAtRegistration(message);}
+
+    @Then("show email warning notification '$message' on Register page")
+    public void verifyEmailWarningAtRegistration(String message){registrationSteps.verifyEmailWarningAtRegistration(message);}
+
+    @Steps
+    ResetPasswordSteps resetPasswordSteps;
+
+    @Given("open reset password page")
+    public void openRessetPage(){resetPasswordSteps.openRessetPage();}
     @When("enter email '$email' in Resset page")
-    public void enterEmailRessetPage(String email){endUser.resetPassword(email);}
+    public void enterEmailRessetPage(String email){resetPasswordSteps.resetPassword(email);}
 
-    @Then("show popup notification '$message'")
-    public void verifyNotification(String message){endUser.verifyNotification(message);}
 }
