52baf2b23d5f5b18ff01727a44fe637e12f5eaa8
Xangel2008 <Xangel2008@gmail.com>
2015-11-03 21:14:02 +0200

    rewrite dataprovider

--- functional_test/src/main/java/ua/com/ua/com/homebudget/Helper.java
+++ functional_test/src/main/java/ua/com/ua/com/homebudget/Helper.java
@@ -2,6 +2,17 @@ package ua.com.ua.com.homebudget;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.DataProvider;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Anohin Artyom on 03.11.15.
@@ -28,12 +39,40 @@ public class Helper {
     }
 
     @DataProvider(name = "dataVerification")
-    public Object[][] dataVerification() {
-        return new Object[][]{
-                {"Exist Email",baseEmail, basePass ,400,"This email is already taken"},
-                {"Empty Email","", generatePass(10), 400, "The email must be between 6 and 60 characters"},
-                {"One Space Email", " ", generatePass(10), 400, "The email must be between 6 and 60 characters"},
+    public Object[][] dataVerification() throws IOException, SAXException, ParserConfigurationException {
+        File fXmlFile = new File("dataprovider_verification.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXmlFile);
 
-        };
+
+        doc.getDocumentElement().normalize();
+
+        NodeList nList = doc.getElementsByTagName("account");
+
+
+        Object[][] array = new Object[nList.getLength()][5];
+
+
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+
+            Node nNode = nList.item(temp);
+
+
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                Element eElement = (Element) nNode;
+
+
+                array[temp][0] = eElement.getElementsByTagName("testnamee").item(0).getTextContent();
+                array[temp][1] = eElement.getElementsByTagName("email").item(0).getTextContent();
+                array[temp][2] = eElement.getElementsByTagName("password").item(0).getTextContent();
+                array[temp][3] = eElement.getElementsByTagName("statuscode").item(0).getTextContent();
+                array[temp][4] = eElement.getElementsByTagName("message").item(0).getTextContent();
+
+
+            }
+        }
+        return array;
     }
 }
