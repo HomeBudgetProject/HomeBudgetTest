bf6bf86571c5aa7a87a6486a2d8e43a285ffa5c5
Xangel2008 <Xangel2008@gmail.com>
2015-11-05 18:39:08 +0200

    some fix in all test

--- functional_test/src/test/java/ua/com/ua/com/homebudget/tests/RegistrationTest.java
+++ functional_test/src/test/java/ua/com/ua/com/homebudget/tests/RegistrationTest.java
@@ -1,6 +1,7 @@
 package ua.com.ua.com.homebudget.tests;
 
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
@@ -8,6 +9,7 @@ import org.testng.annotations.*;
 import ru.yandex.qatools.allure.annotations.Features;
 import ru.yandex.qatools.allure.annotations.Stories;
 import ua.com.ua.com.homebudget.Helper;
+import ua.com.ua.com.homebudget.steps.LoginSteps;
 import ua.com.ua.com.homebudget.steps.RegistrationSteps;
 
 import java.net.MalformedURLException;
@@ -22,69 +24,17 @@ public class RegistrationTest extends Helper{
 
     @Parameters({"browser","platform", "url"})
     @BeforeTest(alwaysRun = true)
-    public void setup(String browser,String platform, String url) throws MalformedURLException {
+    public void setup() throws MalformedURLException {
 
-        if (browser.equalsIgnoreCase("Chrome")) {
-            DesiredCapabilities caps = DesiredCapabilities.chrome();
-            if (platform.equalsIgnoreCase("Windows 7"))
-                caps.setCapability("platform", "Windows 7");
-            if (platform.equalsIgnoreCase("Windows 8"))
-                caps.setCapability("platform", "Windows 8.1");
-            if (platform.equalsIgnoreCase("Windows 10"))
-                caps.setCapability("platform", "Windows 10");
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
+        desiredCapabilities.setVersion(System.getenv("SELENIUM_VERSION"));
+        desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
 
-            caps.setCapability("version", "46.0");
-            caps.setCapability("screenResolution", "1024x768");
-            caps.setCapability("name", "Functional test");
-            registrationStep = new RegistrationSteps(new RemoteWebDriver(new URL(url), caps));
-        }
-        if (browser.equalsIgnoreCase("IE")) {
-            DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
-            if (platform.equalsIgnoreCase("Windows 7")) {
-                caps.setCapability("platform", "Windows 7");
-                caps.setCapability("version", "9.0");
-            }
-            if (platform.equalsIgnoreCase("Windows 8")) {
-                caps.setCapability("platform", "Windows 8");
-                caps.setCapability("version", "10.0");
-            }
-            if (platform.equalsIgnoreCase("Windows 10")) {
-                caps.setCapability("platform", "Windows 10");
-                caps.setCapability("version", "11.0");
-            }
-            caps.setCapability("screenResolution", "1024x768");
-            caps.setCapability("name", "Functional test");
-            registrationStep = new RegistrationSteps(new RemoteWebDriver(new URL(url), caps));
-        }
-        if (browser.equalsIgnoreCase("Opera")) {
-            DesiredCapabilities caps = DesiredCapabilities.opera();
-            caps.setCapability("platform", "Windows 7");
-            caps.setCapability("version", "12.12");
-            caps.setCapability("screenResolution", "1024x768");
-            caps.setCapability("name", "Functional test");
-            registrationStep = new RegistrationSteps(new RemoteWebDriver(new URL(url), caps));
-        }
-        if (browser.equalsIgnoreCase("Firefox")) {
-            DesiredCapabilities caps = DesiredCapabilities.firefox();
-            if (platform.equalsIgnoreCase("Windows 7")) {
-                caps.setCapability("platform", "Windows 7");
-                caps.setCapability("version", "41.0");
-            }
-            if (platform.equalsIgnoreCase("Windows 8")) {
-                caps.setCapability("platform", "Windows 8");
-                caps.setCapability("version", "41.0");
-            }
-            if (platform.equalsIgnoreCase("Windows 10")) {
-                caps.setCapability("platform", "Windows 10");
-                caps.setCapability("version", "41.0");
-            }
-            caps.setCapability("screenResolution", "1024x768");
-            caps.setCapability("name", "Functional test");
-            registrationStep = new RegistrationSteps(new RemoteWebDriver(new URL(url), caps));
-        }
-
+        registrationStep = new RegistrationSteps(new RemoteWebDriver(new URL("http://"+System.getenv("SAUCE_USERNAME")+":"+System.getenv("SAUCE_ACCESS_KEY")+"@ondemand.saucelabs.com:80/wd/hub"), desiredCapabilities));
 
     }
+
 
 
 
