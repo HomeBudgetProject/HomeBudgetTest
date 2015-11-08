6d08751f9074028775e8d246aaed4ca59e817e7e
Xangel2008 <Xangel2008@gmail.com>
2015-11-08 16:15:17 +0200

    fix

--- functional_test/src/test/java/ua/com/ua/com/homebudget/tests/LoginTest.java
+++ functional_test/src/test/java/ua/com/ua/com/homebudget/tests/LoginTest.java
@@ -19,14 +19,14 @@ import java.net.URL;
 public class LoginTest {
     RemoteWebDriver driver;
     private LoginSteps loginSteps;
-    @Parameters({"browser","platform", "url"})
+    @Parameters({"browser","platform", "browserVersion"})
     @BeforeTest(alwaysRun = true)
-    public void setup() throws MalformedURLException {
+    public void setup(String platform, String browser, String browserVersion) throws MalformedURLException {
 
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
-        desiredCapabilities.setVersion(System.getenv("SELENIUM_VERSION"));
-        desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
+        desiredCapabilities.setBrowserName(browser);
+        desiredCapabilities.setVersion(browserVersion);
+        desiredCapabilities.setCapability(CapabilityType.PLATFORM, platform);
         desiredCapabilities.setCapability("name", "Functional test - Login"); //name job in saucelab
         loginSteps = new LoginSteps(new RemoteWebDriver(new URL("http://"+System.getenv("SAUCE_USERNAME")+":"+System.getenv("SAUCE_ACCESS_KEY")+"@ondemand.saucelabs.com:80/wd/hub"), desiredCapabilities));
 
